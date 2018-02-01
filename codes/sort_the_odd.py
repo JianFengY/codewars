8d8a61addce582af726a85a28b329a7f16218edb
杨剑锋 <331387202@qq.com>
2018-02-01 23:20:55 +0800

    Sort the odd

--- codes/sort_the_odd.py
+++ codes/sort_the_odd.py
@@ -24,7 +24,6 @@ def sort_array(source_array):
     return source_array
 
 #     odds = sorted((x for x in source_array if x % 2 != 0), reverse=True)
-#     print(odds)
 #     return [x if x % 2 == 0 else odds.pop() for x in source_array]
 
 
