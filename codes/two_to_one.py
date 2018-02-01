35df458efda54cb67810c5fca46ee05c294cf65b
杨剑锋 <331387202@qq.com>
2018-02-01 16:11:20 +0800

    Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters,each taken only once - coming from s1 or s2.

---
+++ codes/two_to_one.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2018年2月1日
+
+@author: Jeff Yang
+'''
+
+'''
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, 
+the longest possible, containing distinct letters,each taken only once - coming from s1 
+or s2. 
+Examples: a = "xyaabbbccccdefww" b = "xxxxyyyyabklmopq" longest(a, b) -> "abcdefklmopqwxy"
+a = "abcdefghijklmnopqrstuvwxyz" longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+'''
+
+
+def longest(s1, s2):
+    """sort and distinct letters"""
+    s = sorted(set(s1 + s2))
+    return ''.join(s)
+
+
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+print(longest(a, b))
