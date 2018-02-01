f5e0c5b90188c6162bf60ad6c4911b0bbe3660e0
杨剑锋 <331387202@qq.com>
2018-02-02 01:57:06 +0800

    Equal Sides Of An Array

--- codes/equal_sides_of_an_array.py
+++ codes/equal_sides_of_an_array.py
@@ -21,6 +21,9 @@ def find_even_index(arr):
             return i
     return -1
 
+#     r = [i for i in range(len(arr)) if sum(arr[0:i]) == sum(arr[i + 1:])]
+#     return r[0] if r else -1
+
 
 print(find_even_index([1, 2, 3, 4, 3, 2, 1]))
 print(find_even_index([1, 100, 50, -51, 1, 1]))
