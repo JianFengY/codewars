71aef0020cc985dca30d0f37302ca7494b7c4333
杨剑锋 <331387202@qq.com>
2018-02-02 19:10:01 +0800

    Format a string of names like 'Bart, Lisa & Maggie'

---
+++ codes/format_a_string_of_names.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2018年2月2日
+
+@author: Jeff Yang
+'''
+
+'''
+Given: an array containing hashes of names
+Return: a string formatted as a list of names separated by commas except for the 
+last two names, which should be separated by an ampersand.
+Example:
+namelist([ {'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'} ])  #'Bart, Lisa & Maggie'
+namelist([ {'name': 'Bart'}, {'name': 'Lisa'} ])  # 'Bart & Lisa'
+namelist([ {'name': 'Bart'} ])  # 'Bart'
+namelist([])  # ''
+'''
+
+
+def namelist(names):
+    """format names with commas and ampersand"""
+    return '' if len(names) == 0 else ', '.join([x['name'] for x in names[:-1]]) + \
+        ' & ' + names[-1]['name'] if len(names) > 1 else names[0]['name']
+
+#     return ", ".join([name["name"] for name in names])[::-1].replace(",", "& ", 1)[::-1]
+
+
+print(namelist([{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}, {'name': 'Homer'}, {'name': 'Marge'}]))
+print(namelist([{'name': 'Bart'}, {'name': 'Lisa'}, {'name': 'Maggie'}]))
+print(namelist([{'name': 'Bart'}, {'name': 'Lisa'}]))
+print(namelist([{'name': 'Bart'}]))
+print(namelist([]))
